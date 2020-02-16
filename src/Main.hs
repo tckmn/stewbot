b810d020562e079ee3305d97e1fa8323e326d926
Andy Tockman <andy@tck.mn>
2020-02-16 11:52:08 -0500

    wow it's gonna be a server

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 import Stewbot
-import qualified Network.Wreq.Session as S
 import System.Process
 
-main = do
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.HTTP.Types (status200, status404)
+import Text.Hamlet
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Data.Text.Lazy.Encoding (encodeUtf8)
+
+main = makeBot >>= run 3000 . app
+
+resp :: Html -> Response
+resp = responseLBS status200 [("Content-Type", "text/html")] . encodeUtf8 . renderHtml
+
+notfound :: Response
+notfound = responseLBS status404 [("Content-Type", "text/plain")] "not found"
+
+headContent = [shamlet|
+    <title>Stewbot
+    <meta charset='utf-8'>
+    |]
+
+header = [shamlet|
+    <p>HEADER
+    |]
+
+app bot req respond = respond $
+    case requestMethod req of
+      "GET"  -> case pathInfo req of
+                  [] -> resp $(shamletFile "html/home.hamlet")
+                  _ -> notfound
+      "POST" -> case pathInfo req of
+                  _ -> notfound
+
+-- oldstuffs
+
+oldmain = do
     bot <- makeBot
     runSearch bot "list" >>= writeFile "out.html"
     callCommand "firefox out.html"
 
-oldmain = do
+oldoldmain = do
     bot <- makeBot
     res <- addItems bot
         [Item "item_79003957"   1 Best
